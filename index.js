287eddea4f3676be73cfb0e1fc8294ed76a6238c
dongchu627 <dongchu627@github.com>
2021-02-06 15:55:36 +0900

    Theory Finish

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const title = document.querySelector("#h1");
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "rgb(100, 50, 0";
-
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
-    }
-        
+    const currentClass = title.className;
+    
 }
+        
 
 function init() {
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 
 }
