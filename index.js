9a3d237cff3fa7413f0c22a76be20bd9e0c8d42a
dongchu627 <dongchu627@github.com>
2021-02-06 15:37:09 +0900

    on click event

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-if(10 === 5) {
-    console.log("he")
+const title = document.querySelector("#h1");
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "rgb(100, 50, 0";
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    }
 }
-else{
-    console.log("z")
+
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
 
 }
+
+init();
